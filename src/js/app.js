715de36b200dd3e9cb689d90ed05965f5d954e0b
Vignesh Damodharan <mailstovignesh@gmail.com>
2016-05-12 10:21:38 +0530

    Working prototype

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,8 @@
-angular.module('superdupertodoApp', [])
-  .controller('TodoListController', function() {
-    var todoList = this;
+var app = angular.module('superdupertodoApp', []);
+app.controller('TodoListController',function(){
+   var todoList = this;
     todoList.todos = [
       {text:'learn angular', done:false},
       {text:'learn git', done:false},
       {text:'build an angular app', done:false}];
-    };
-  });
+});
